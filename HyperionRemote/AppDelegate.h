0732acb738e5e773c57c961b04e96f841f2d5cc0
Tobias Tangemann <tobiastangemann@gmail.com>
2015-08-14 22:16:02 +0200

    Initial Commit

---
+++ HyperionRemote/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HyperionRemote
+//
+//  Created by Tobias Tangemann on 14.08.15.
+//  Copyright (c) 2015 Tobias Tangemann. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
